ce97c83a740f78fe7bf4ec9b7d5d7f21b8a65193
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-09-10 02:03:25 +0500

    leads added

---
+++ src/app/leads/page.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import React from 'react'
+
+
+const getData = async () => {
+    const resposne = await axios.get(`${process.env.DOMAIN}/api/contacts`);
+    return resposne.data.contacts;
+}
+
+const LeadsPage = async () => {
+    const data = await getData();
+    // console.log("ali", data);
+  return (
+    <div className="min-h-[500px] w-full flex items-center justify-center text-black py-10 overflow-hidden">
+    <div className="overflow-auto border border-black">
+      <table className="table">
+        <thead>
+          <tr className="">
+            <th>Name</th>
+            <th>Email</th>
+            <th>phone</th>
+            <th>Message</th>                    
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item) => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td>{item.message}</td>              
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
+  )
+}
+
+export default LeadsPage
