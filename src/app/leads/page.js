86ca2491cf31040de51d27799031926cbd326b4b
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-09-10 04:21:24 +0500

    final

--- src/app/leads/page.js
+++ src/app/leads/page.js
@@ -1,23 +1,45 @@
+"use client";
 import axios from 'axios'
-import React from 'react'
-import LeadsTable from './LeadsTable';
+import React, { useEffect, useState } from 'react'
 
 
-const getData = async () => {
-    const resposne = await axios.get(`${process.env.DOMAIN}api/contacts`);
-    return resposne.data.contacts;
-
-    // let response = await fetch(`${process.env.DOMAIN}api/contacts`, { next: { revalidate: 1 } });
-    // response = await response.json();
-    // return response.contacts;
-}
-
-const LeadsPage = async () => {
-    const data = await getData();
-    // console.log("ali", data);
+const LeadsPage = () => {
+  const [data, setData] = useState([]);
+  
+  async function getData (){
+    const resposne = await axios.get(`/api/contacts`);
+    // return resposne.data.contacts;
+    setData(resposne.data.contacts);
+  }
+  useEffect(() => {
+    getData();
+  }, [])
+  
+  console.log(data);
   return (
     <div className="min-h-[500px] w-full flex items-center justify-center text-black py-10 overflow-hidden   ">
-    <LeadsTable data={data} />
+    <div className="overflow-auto border border-black">
+      <table className="table">
+        <thead>
+          <tr className="">
+            <th>Name</th>
+            <th>Email</th>
+            <th>phone</th>
+            <th>Message</th>                    
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item) => (
+            <tr key={item._id}>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+              <td>{item.message}</td>              
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   </div>
   )
 }
