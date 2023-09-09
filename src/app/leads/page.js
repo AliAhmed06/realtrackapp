c89fa82b16df0c0ebe1c17013edded3ddfacfb9b
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-09-10 02:32:48 +0500

    leads error 2

--- src/app/leads/page.js
+++ src/app/leads/page.js
@@ -3,13 +3,17 @@ import React from 'react'
 
 
 const getData = async () => {
-    const resposne = await axios.get(`${process.env.DOMAIN}/api/contacts`);
-    return resposne.data.contacts;
+    // const resposne = await axios.get(`${process.env.DOMAIN}/api/contacts`);
+    // return resposne.data.contacts;
+
+    let response = await fetch(`${process.env.DOMAIN}/api/contacts`, { cache: 'no-store' });
+    response = await response.json();
+    return response.contacts;
 }
 
 const LeadsPage = async () => {
     const data = await getData();
-    // console.log("ali", data);
+    console.log("ali", data);
   return (
     <div className="min-h-[500px] w-full flex items-center justify-center text-black py-10 overflow-hidden   ">
     <div className="overflow-auto border border-black">
