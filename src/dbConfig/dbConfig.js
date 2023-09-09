cf2fb0b3b2d903d808f205239ee350d00e69f3bb
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-09-10 03:14:21 +0500

    error11

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -46,7 +46,7 @@ async function connectDB() {
 
   if (!cached.promise) {
     const opts = {
-      bufferCommands: false
+      bufferCommands: true
     }
 
     cached.promise = connect(MONGODB_URI, opts)
